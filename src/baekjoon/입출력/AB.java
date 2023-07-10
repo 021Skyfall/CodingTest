9b4f6c83559ada6e8c2dacb360f399cae9ffaa1a
021Skyfall <021skyfall@gmail.com>
2023-07-10 18:47:36 +0900

    day 22 - BaekJoon 시작 / 입출력 파트 50%

---
+++ src/baekjoon/입출력/AB.java
@@ -0,0 +1,42 @@
+package baekjoon.입출력;
+
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AB {
+    public static void main(String[] args) throws IOException {
+        firstCal();
+
+        secondaryCal();
+    }
+
+    private static void firstCal()  throws IOException {
+        int a = System.in.read() - '0';
+        System.in.read();
+        int b = System.in.read() - '0';
+        calculate(a, b);
+    }
+
+    private static void secondaryCal() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        calculate(a, b);
+    }
+
+    private static void calculate(int a, int b) {
+        long start = System.nanoTime();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        long end = System.nanoTime();
+        System.out.println(end - start + " ns");
+    }
+}
+
+// BufferReader 사용 실패
+// https://st-lab.tistory.com/18 -> BufferReader 사용법
---
+++ src/baekjoon/입출력/HelloWord.java
@@ -0,0 +1,8 @@
+package baekjoon.입출력;
+
+// 백준에 제출할 때 클래스 메인으로
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
