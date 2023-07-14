a997ddc724895f2e5ff3363d0672f531f0542bb5
021Skyfall <021skyfall@gmail.com>
2023-07-14 19:55:08 +0900

    day 26 - BaekJoon / 조건문 파트 80%

---
+++ src/baekjoon/조건/Alarm.java
@@ -0,0 +1,25 @@
+package baekjoon.조건;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Alarm {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String time = br.readLine();
+        String[] result = time.split(" ");
+        int h = Integer.parseInt(result[0]);
+        int m = Integer.parseInt(result[1]);
+        if (m < 45) {
+            if (h == 0) h = 23;
+            else h--;
+            m += 15;
+        } else {
+            m -= 45;
+        }
+        if (m == 60)
+            m = 0;
+        System.out.println(h + " " + m);
+        }
+    }
+}
---
+++ src/baekjoon/조건/Quadrant.java
@@ -0,0 +1,20 @@
+package baekjoon.조건;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Quadrant {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        if (a*b > 0) {
+            if (a > 0) System.out.println(1);
+            else System.out.println(3);
+        } else if (a*b < 0) {
+            if (a > 0) System.out.println(4);
+            else System.out.println(2);
+        }
+    }
+}
