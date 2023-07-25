30464dbb26340ea9853446fcce3a9d9798e03a2e
021Skyfall <021skyfall@gmail.com>
2023-07-25 13:51:48 +0900

    day 33 - BaekJoon / 배열 파트 50%

--- src/baekjoon/배열/BJ_10810.java
+++ src/baekjoon/배열/BJ_10810.java
@@ -2,27 +2,44 @@ package baekjoon.배열;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BJ_10810 {
     public static void main(String[] args) throws java.io.IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] total = br.readLine().split(" ");
-        int[] basket = new int[Integer.parseInt(total[0])];
-        int count = Integer.parseInt(total[1]);
+//        String[] total = br.readLine().split(" ");
+//        int[] basket = new int[Integer.parseInt(total[0])];
+//        int count = Integer.parseInt(total[1]);
+//
+//        for (int i = 0; i < count; i++) {
+//            String[] nums = br.readLine().split(" ");
+//            int ball = Integer.parseInt(nums[2]);
+//
+//            for (int j = Integer.parseInt(nums[0]) - 1; j < Integer.parseInt(nums[1]); j++) {
+//                basket[j] = ball;
+//            }
+//        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < count; i++) {
-            String[] nums = br.readLine().split(" ");
-            int ball = Integer.parseInt(nums[2]);
+        int[] basket = new int[N];
 
-            for (int j = Integer.parseInt(nums[0]) - 1; j < Integer.parseInt(nums[1]); j++) {
-                basket[j] = ball;
+        for (int l = 0; l < M; l++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int idx = i - 1; idx < j; idx++) {
+                basket[idx] = k;
             }
         }
 
         br.close();
 
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < basket.length; i++) {
+        for (int i = 0; i < N; i++) {
 
             result.append(basket[i]).append(" ");
         }
