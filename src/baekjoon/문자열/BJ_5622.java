83f05d88e0fb89deb6b380d5af2cc77084750f4b
021Skyfall <021skyfall@gmail.com>
2023-08-01 20:34:39 +0900

    day 38 - BaekJoon / 문자열 파트 90%

---
+++ src/baekjoon/문자열/BJ_5622.java
@@ -0,0 +1,16 @@
+package baekjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_5622 {
+    public static void main(String[] args) throws java.io.IOException {
+        // 문자 -> 다이얼 숫자
+        // 종료 후 문자열 길이 +
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split("");
+
+
+    }
+}
