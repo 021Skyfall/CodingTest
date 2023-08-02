5521c3410f0ead2816abb61071ffda8f682887c3
021Skyfall <021skyfall@gmail.com>
2023-08-02 15:42:47 +0900

    day 38 - BaekJoon / 문자열 파트 100%

---
+++ src/baekjoon/문자열/BJ_11718.java
@@ -0,0 +1,14 @@
+package baekjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_11718 {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
--- src/baekjoon/문자열/BJ_5622.java
+++ src/baekjoon/문자열/BJ_5622.java
@@ -5,12 +5,40 @@ import java.io.InputStreamReader;
 
 public class BJ_5622 {
     public static void main(String[] args) throws java.io.IOException {
-        // 문자 -> 다이얼 숫자
-        // 종료 후 문자열 길이 +
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split("");
+        String s = br.readLine();
 
+        int count = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'A' : case 'B' : case 'C' :
+                    count += 3;
+                    break;
+                case 'D' : case 'E' : case 'F' :
+                    count += 4;
+                    break;
+                case 'G' : case 'H' : case 'I' :
+                    count += 5;
+                    break;
+                case 'J' : case 'K' : case 'L' :
+                    count += 6;
+                    break;
+                case 'M' : case 'N' : case 'O' :
+                    count += 7;
+                    break;
+                case 'P' : case 'Q' : case 'R' : case 'S' :
+                    count += 8;
+                    break;
+                case 'T' : case 'U' : case 'V' :
+                    count += 9;
+                    break;
+                case 'W' : case 'X' : case 'Y' : case 'Z' :
+                    count += 10;
+                    break;
+            }
+        }
+
+        System.out.println(count);
     }
 }
