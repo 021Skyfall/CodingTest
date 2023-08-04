da53194ab87fe65d549f236c69adcb3a415f9fb5
021Skyfall <021skyfall@gmail.com>
2023-08-04 23:27:17 +0900

    fix

---
+++ src/study/DoublePriorityQueue.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.util.Arrays;
+
+public class DoublePriorityQueue {
+    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(solution()));
+    }
+    private static int[] solution(String[] operations) {
+        int[] answer = {};
+        return answer;
+    }
+}
