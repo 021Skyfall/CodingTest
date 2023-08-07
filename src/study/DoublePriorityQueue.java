5dea8fbb8b450fc456e113d72e1f4fc451c18230
021Skyfall <021skyfall@gmail.com>
2023-08-07 14:14:46 +0900

    1. 이중우선순위큐 / 평균 0.55ms / 정확성 100.0

--- src/study/DoublePriorityQueue.java
+++ src/study/DoublePriorityQueue.java
@@ -1,13 +1,44 @@
 package study;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class DoublePriorityQueue {
     public static void main(String[] args) {
-//        System.out.println(Arrays.toString(solution()));
+        System.out.println(Arrays.toString(solution(new String[]{"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"})));
     }
     private static int[] solution(String[] operations) {
-        int[] answer = {};
-        return answer;
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxQ = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for (int i = 0; i < operations.length; i++) {
+            String[] arr = operations[i].split(" ");
+            if (arr[0].equals("I")) {
+                int num = Integer.parseInt(arr[1]);
+                minQ.add(num);
+                maxQ.add(num);
+            } else if (arr[0].equals("D")) {
+                if (arr[1].equals("1")) {
+                    if (!minQ.isEmpty()) {
+                        int num = maxQ.poll();
+                        minQ.remove(num);
+                    }
+                } else if (arr[1].equals("-1")) {
+                    if (!minQ.isEmpty()) {
+                        int num = minQ.poll();
+                        maxQ.remove(num);
+                    }
+                }
+            }
+        }
+
+        if (minQ.isEmpty() && maxQ.isEmpty()) {
+            return new int[]{0, 0};
+        } else {
+            int max = maxQ.isEmpty() ? 0 : maxQ.poll();
+            int min = minQ.isEmpty() ? 0 : minQ.poll();
+            return new int[]{max, min};
+        }
     }
 }
