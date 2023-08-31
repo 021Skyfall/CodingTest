6c7ec649cbe459c3402d1a00ad7f7d45b65e1135
021Skyfall <021skyfall@gmail.com>
2023-08-31 17:57:25 +0900

    day 52 - BaekJoon / 일반 수학1 파트 90%

--- src/baekjoon/일반수학1/BJ_1193.java
+++ src/baekjoon/일반수학1/BJ_1193.java
@@ -7,6 +7,24 @@ public class BJ_1193 {
     public static void main(String[] args) throws java.io.IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int x = Integer.parseInt(br.readLine());
 
+        int crs_count = 1, prev_count_sum = 0;
+
+        while (true) {
+            if (x <= prev_count_sum + crs_count) {
+                if (crs_count % 2 == 1) {
+                    System.out.println((crs_count - (x - prev_count_sum - 1)) + "/" + (x - prev_count_sum));
+                    break;
+                }
+                else {
+                    System.out.println((x - prev_count_sum) + "/" + (crs_count - (x - prev_count_sum - 1)));
+                    break;
+                }
+            } else {
+                prev_count_sum += crs_count;
+                crs_count++;
+            }
+        }
     }
 }
