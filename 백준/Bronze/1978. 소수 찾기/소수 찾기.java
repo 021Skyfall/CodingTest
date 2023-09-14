e30da30fa21eb734b1d9f71860c403bbfe45e136
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-09-14 18:19:22 +0900

    [Bronze II] Title: 소수 찾기, Time: 120 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+        int count = 0;
+        br.close();
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(arr[i]);
+            if (x == 1) continue;
+            if (isPrime(x)) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+
+        for (int i = 3; i*i <= num; i += 2) {
+            if (num % i == 0) return false;
+        }
+
+        return true;
+    }
+}
