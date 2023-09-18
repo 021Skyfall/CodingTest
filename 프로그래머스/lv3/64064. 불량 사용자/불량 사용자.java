82af0b125d244f433d7e75d6afa6b36362abd345
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-09-18 14:44:36 +0900

    [level 3] Title: 불량 사용자, Time: 0.43 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -1,29 +1,39 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
-
-    Set<Integer> set;
-
     public int solution(String[] user_id, String[] banned_id) {
-        set = new HashSet<>();
-
-        go(0, user_id, banned_id, 0);
-
-        return set.size();
+        Set<Set<String>> validComb = new HashSet<>();
+        findValidCombinations(user_id, banned_id, 0, new HashSet<>(), validComb);
+        return validComb.size();
     }
 
-    public void go(int index, String[] user_id, String[] banned_id, int bit) {
-
-        if(index == banned_id.length) {
-            set.add(bit);
+    public void findValidCombinations(String[] user_id, String[] banned_id, int index, Set<String> curComb, Set<Set<String>> validComb) {
+        if (index == banned_id.length) {
+            validComb.add(new HashSet<>(curComb));
             return;
         }
 
-        String reg = banned_id[index].replace("*", "[\\w\\d]");
-        for(int i=0; i<user_id.length; ++i) {
-            if((((bit>>i) & 1) == 1) || !user_id[i].matches(reg)) continue;
-            go(index + 1, user_id, banned_id, (bit | 1<<i));
+        String currentBannedId = banned_id[index];
+        for (String userId : user_id) {
+            if (!curComb.contains(userId) && isValid(userId, currentBannedId)) {
+                curComb.add(userId);
+                findValidCombinations(user_id, banned_id, index + 1, curComb, validComb);
+                curComb.remove(userId);
+            }
         }
+    }
+
+    public boolean isValid(String user, String banned) {
+        if (user.length() != banned.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < user.length(); i++) {
+            if (banned.charAt(i) != '*' && banned.charAt(i) != user.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
