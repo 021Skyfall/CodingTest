84361353c9c1cd909bdbddb6ccfb1da7a301ac1a
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-09-18 14:29:16 +0900

    [level 3] Title: 불량 사용자, Time: 4.18 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -3,46 +3,27 @@ import java.util.Set;
 
 public class Solution {
 
+    Set<Integer> set;
+
     public int solution(String[] user_id, String[] banned_id) {
-        Set<Set<String>> validCombinations = new HashSet<>();
-        generateValidCombinations(user_id, banned_id, 0, new HashSet<>(), validCombinations);
-        return validCombinations.size();
+        set = new HashSet<>();
+
+        go(0, user_id, banned_id, 0);
+
+        return set.size();
     }
 
-    public void generateValidCombinations(String[] user_id, String[] banned_id, int index, Set<String> currentCombination, Set<Set<String>> validCombinations) {
-        if (index == banned_id.length) {
-            if (currentCombination.size() == banned_id.length) {
-                validCombinations.add(new HashSet<>(currentCombination));
-            }
+    public void go(int index, String[] user_id, String[] banned_id, int bit) {
+
+        if(index == banned_id.length) {
+            set.add(bit);
             return;
         }
 
-        String currentBannedId = banned_id[index];
-        for (String userId : user_id) {
-            if (!currentCombination.contains(userId) && IdLength(userId, currentBannedId) && compare(userId, currentBannedId)) {
-                currentCombination.add(userId);
-                generateValidCombinations(user_id, banned_id, index + 1, currentCombination, validCombinations);
-                currentCombination.remove(userId);
-            }
+        String reg = banned_id[index].replace("*", "[\\w\\d]");
+        for(int i=0; i<user_id.length; ++i) {
+            if((((bit>>i) & 1) == 1) || !user_id[i].matches(reg)) continue;
+            go(index + 1, user_id, banned_id, (bit | 1<<i));
         }
-    }
-
-    public boolean compare(String x, String y) {
-        int count = 0;
-        int leng = 0;
-        for (int i = 0; i < x.length(); i++) {
-            if (x.charAt(i) == y.charAt(i)) {
-                count++;
-            }
-        }
-        for (int i = 0; i < y.length(); i++) {
-            if (y.charAt(i) != '*')
-                leng++;
-        }
-        return count == leng;
-    }
-
-    private static boolean IdLength(String x, String y) {
-        return x.length() == y.length();
     }
 }
