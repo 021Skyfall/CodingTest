6b1019402b77a92a3e2f2b19a6dd330998763900
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-09-20 17:46:15 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min1 = Math.min(x, w-x);
+        int min2 = Math.min(y, h-y);
+
+        System.out.println(Math.min(min1,min2));
+    }
+}
