6f7bf19e1314a130e5f1b77cb74deea9b8156e6b
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-10-03 17:03:19 +0900

    [Bronze II] Title: 커트라인, Time: 140 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[n];
+
+        st = new StringTokenizer(br.readLine());
+        br.close();
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(arr[k-1]);
+    }
+}
