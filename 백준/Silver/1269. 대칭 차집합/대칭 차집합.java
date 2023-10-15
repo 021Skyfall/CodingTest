00a5313559f1d27f2cef70783a23570ae9fcfa8e
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-10-15 13:21:56 +0900

    [Silver IV] Title: 대칭 차집합, Time: 724 ms, Memory: 83608 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws java.io.IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -12,30 +12,21 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            set1.add(Integer.parseInt(st.nextToken()));
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
         st = new StringTokenizer(br.readLine());
-        HashSet<Integer> set2 = new HashSet<>();
-
-        int unique2 = 0;
         for (int i = 0; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (!set1.contains(num)) {
-                unique2++;
-            }
-            set2.add(num);
-        }
-
-        int unique1 = 0;
-        for (int num : set1) {
-            if (!set2.contains(num)) {
-                unique1++;
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
             }
         }
 
-        System.out.println(unique1+unique2);
+        System.out.println(set.size());
     }
 }
