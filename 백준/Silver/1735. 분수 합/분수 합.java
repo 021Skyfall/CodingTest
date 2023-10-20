c8f46d4b1fffc684b63664cd8c3a5efcf16b1c61
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-10-20 12:38:25 +0900

    [Silver III] Title: 분수 합, Time: 156 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int b1 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int a2 = Integer.parseInt(st.nextToken());
+        int b2 = Integer.parseInt(st.nextToken());
+
+        int resultA = (a1 * b2) + (a2 * b1);
+        int resultB = b1 * b2;
+
+        int gcd = gcd(resultA, resultB);
+        System.out.println(resultA/gcd + " " + resultB/gcd);
+    }
+    public static int gcd(int x, int y) {
+        while (y == 0) {
+            return x;
+        }
+        return gcd(y, x%y);
+    }
+}
