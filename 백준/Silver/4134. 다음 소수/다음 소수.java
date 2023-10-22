505c5bce4eeadb5160b1a8a8359624ce220e05d2
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-10-22 16:47:55 +0900

    [Silver IV] Title: 다음 소수, Time: 444 ms, Memory: 14768 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            long x = Long.parseLong(br.readLine());
+            while(true) {
+                if (isPrime(x)) {
+                    System.out.println(x);
+                    break;
+                }
+                x++;
+            }
+        }
+    }
+    public static boolean isPrime(long x) {
+        if (x < 2) return false;
+        for (long i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
