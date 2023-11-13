a4ce5f32267a2d469434c34a39dd48eb1f5bea63
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-11-13 11:11:31 +0900

    [Gold V] Title: 토마토, Time: 668 ms, Memory: 120280 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr;
+    static int n,m;
+
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        bfs(q);
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    System.out.println("-1");
+                    return;
+                }
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+
+        System.out.println(max - 1);
+    }
+
+    public static void bfs(Queue<int[]> q) {
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (arr[nx][ny] == 0) {
+                        q.offer(new int[]{nx, ny});
+                        arr[nx][ny] = arr[cur[0]][cur[1]] + 1;
+                    }
+                }
+            }
+        }
+    }
+}
