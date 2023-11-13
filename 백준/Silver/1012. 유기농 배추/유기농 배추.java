a0f1d2359bcd2e361f0abac6730a70e3c9847661
021Skyfall <021skyfall@gmail.com>
2023-11-13 10:41:49 +0900

    백준 1012번 유기농 배추 BFS 탐색 알고리즘 추가

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -35,7 +37,8 @@ public class Main {
                 for (int j = 0; j < m; j++) {
                     if (arr[i][j] == 1 && !visited[i][j]) {
                         count++;
-                        dfs(i,j);
+                        bfs(i,j);
+//                        dfs(i,j);
                     }
                 }
             }
@@ -45,6 +48,28 @@ public class Main {
         }
     }
 
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        visited[x][y] = true;
+        q.offer(new int[]{x,y});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (arr[nx][ny] == 1 && !visited[nx][ny]) {
+                        q.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // dfs 탐색
     public static void dfs(int x, int y) {
         visited[x][y] = true;
 
