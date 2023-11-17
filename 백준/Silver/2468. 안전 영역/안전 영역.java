15684724e397201a43a743bec38a189cf91d5e5d
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-11-17 10:29:57 +0900

    [Silver I] Title: 안전 영역, Time: 284 ms, Memory: 18944 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int n, max = 0;
+
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+
+        int safeArea = 0;
+        for (int i = 0; i <= max; i++) {
+            visited = new boolean[n][n];
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (arr[j][k] > i && !visited[j][k]) {
+                        count++;
+                        dfs(i,j,k);
+                    }
+                }
+            }
+            safeArea = Math.max(safeArea, count);
+        }
+
+        System.out.println(safeArea);
+    }
+
+    public static void dfs(int i, int x, int y) {
+        visited[x][y] = true;
+
+        for (int l = 0; l < 4; l++) {
+            int nx = x + dx[l];
+            int ny = y + dy[l];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                if (arr[nx][ny] > i && !visited[nx][ny]) {
+                    dfs(i, nx, ny);
+                }
+            }
+        }
+    }
+}
