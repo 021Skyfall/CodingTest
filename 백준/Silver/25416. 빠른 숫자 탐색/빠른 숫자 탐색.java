83f2a3933a8b0ad4dd11f630978f7b11883d6350
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2024-01-02 10:18:44 +0900

    [Silver II] Title: 빠른 숫자 탐색, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/25416. 빠른 숫자 탐색/빠른 숫자 탐색.java
+++ 백준/Silver/25416. 빠른 숫자 탐색/빠른 숫자 탐색.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int r, c, count = 0;
+    static int r, c;
     static int[][] arr;
     static boolean[][] visited;
     static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
