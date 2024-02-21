31e08a882d615ea17f197c90d3112de7ba678ab4
jerry <021skyfall@gmail.com>
2024-02-21 10:45:11 +0900

    백준 2217 : 로프

--- src/baekjoon/정렬/로프.java
+++ src/baekjoon/정렬/로프.java
@@ -1,2 +1,30 @@
-package baekjoon.정렬;public class 로프 {
+package baekjoon.정렬;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 로프 {
+    public static void main(String[] args) throws java.io.IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Integer[] ropes = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            ropes[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(ropes, Collections.reverseOrder());
+
+        int w = 0;
+        for (int i = 0; i < ropes.length; i++) {
+            if (w < ropes[i] * (i + 1)) {
+                w = ropes[i] * (i + 1);
+            }
+        }
+
+        System.out.println(w);
+    }
 }
