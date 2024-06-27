bd7d6ea8e64f1a31fd73c3a4df656d5c46f56cf0
jerry <021skyfall@gmail.com>
2024-06-27 10:11:21 +0900

    pr : elevator

--- src/Programmers/lvl2/dynamic/elevator.java
+++ src/Programmers/lvl2/dynamic/elevator.java
@@ -1,2 +1,30 @@
-package Programmers.lvl2.dynamic;public class elevator {
+package Programmers.lvl2.dynamic;
+
+public class elevator {
+    public static void main(String[] args) {
+        int storey = 2554;
+        int result = solution(storey);
+        System.out.println("최소 이동 횟수: " + result);
+    }
+
+    private static int solution(int storey) {
+        if (storey == 0) {
+            return 0;
+        }
+
+        int lastDigit = storey % 10;
+        int remaining = storey / 10;
+
+        if (lastDigit > 5) {
+            return (10 - lastDigit) + solution(remaining + 1);
+        } else if (lastDigit == 5) {
+            if ((remaining % 10) >= 5) {
+                return 5 + solution(remaining + 1);
+            } else {
+                return 5 + solution(remaining);
+            }
+        } else {
+            return lastDigit + solution(remaining);
+        }
+    }
 }
