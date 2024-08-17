5146a725d6442855a92a42d52529390f47bd58d8
jerry <021skyfall@gmail.com>
2024-08-17 11:45:55 +0900

    pr : SumNums

---
+++ src/Programmers/lvl1/SumNums.java
@@ -0,0 +1,17 @@
+package Programmers.lvl1;
+
+public class SumNums {
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+    }
+
+    public static int solution(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
