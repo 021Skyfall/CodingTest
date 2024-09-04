501707236a5f54d2d3c99619c0650d295cdeb407
jerry <021skyfall@gmail.com>
2024-09-04 17:53:51 +0900

    pr : PasswordOfTwo

---
+++ src/Programmers/lvl2/PasswordOfTwo.java
@@ -0,0 +1,32 @@
+package Programmers.lvl1;
+
+public class PasswordOfTwo {
+    public static void main(String[] args) {
+        System.out.println(solution("aukks","wbqd",5));
+    }
+
+    public static String solution(String s, String skip, int index) {
+        StringBuilder result = new StringBuilder();
+        boolean[] skipSet = new boolean[26];
+
+        for (char c : skip.toCharArray()) {
+            skipSet[c - 'a'] = true;
+        }
+
+        for (char ch : s.toCharArray()) {
+            char cur = ch;
+
+            for (int i = 0; i < index; i++) {
+                do {
+                    cur++;
+                    if (cur > 'z')
+                        cur = 'a';
+                } while (skipSet[cur - 'a']);
+            }
+
+            result.append(cur);
+        }
+
+        return result.toString();
+    }
+}
